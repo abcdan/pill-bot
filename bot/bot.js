cd2a4e874b0b4d5103b4f3b7be94a1b3cc98a52e
Daniel <daniel@lngzl.nl>
2021-04-12 23:14:45 +0200

    done

--- bot/bot.js
+++ bot/bot.js
@@ -17,17 +17,18 @@ const userWhitelist = [PERSON_TO_REMIND, PERSON_TO_WARN]
 
 bot.onText(/confirm/, (msg) => {
   const chatId = msg.chat.id
-
-  // const lastConfirm = getLastConfirm()
+  if (!userWhitelist.includes(chatId)) { return }
 
   const now = getDate()
   setLastConfirm(now)
 
+  bot.sendMessage(PERSON_TO_WARN, '✅ She confirmed, you can hit it again safely!')
   bot.sendMessage(chatId, '✅ Confirmed, see you tomorrow!')
 })
 
 bot.onText(/last/, (msg) => {
   const chatId = msg.chat.id
+  if (!userWhitelist.includes(chatId)) { return }
 
   const lastConfirm = getLastConfirm()
   const agoTime = moment(lastConfirm.toUTCString()).fromNow()
@@ -35,21 +36,62 @@ bot.onText(/last/, (msg) => {
   bot.sendMessage(chatId, `⌚ The last time your confirmed was about ${agoTime}`, { parse_mode: 'MarkdownV2' })
 })
 
-const job = new CronJob('* 21 * * *', () => {
+/** Prachtig stukje clean code die gerefactored mag worden */
+const job = new CronJob('0 21 * * *', () => {
   bot.sendMessage(PERSON_TO_REMIND, '⚠ Take your pill and /confirm it for me')
 }, null, true, 'Europe/Amsterdam')
 
 job.start()
 
+const job2 = new CronJob('0 22 * * *', () => {
+  const now = getDate()
+  const last = getLastConfirm()
+
+  const lastConfirmDay = moment(last).dayOfYear()
+  const todayDay = moment(now).dayOfYear()
+
+  if (lastConfirmDay === todayDay) { return }
+  bot.sendMessage(PERSON_TO_REMIND, '⚠ Take your pill and /confirm it for me')
+}, null, true, 'Europe/Amsterdam')
+
+job2.start()
+
+const job3 = new CronJob('0 23 * * *', () => {
+  const now = getDate()
+  const last = getLastConfirm()
+
+  const lastConfirmDay = moment(last).dayOfYear()
+  const todayDay = moment(now).dayOfYear()
+
+  if (lastConfirmDay === todayDay) { return }
+  bot.sendMessage(PERSON_TO_REMIND, '⚠ Take your pill and /confirm it for me')
+}, null, true, 'Europe/Amsterdam')
+
+job3.start()
+
+const job4 = new CronJob('50 23 * * *', () => {
+  const now = getDate()
+  const last = getLastConfirm()
+
+  const lastConfirmDay = moment(last).dayOfYear()
+  const todayDay = moment(now).dayOfYear()
+
+  if (lastConfirmDay === todayDay) return
+
+  const warning = '🚨 OH NO, LAST REMINDER 🚨'
+  bot.sendMessage(PERSON_TO_REMIND, warning)
+  bot.sendMessage(PERSON_TO_WARN, warning)
+  bot.sendMessage(PERSON_TO_REMIND, warning)
+  bot.sendMessage(PERSON_TO_WARN, warning)
+  bot.sendMessage(PERSON_TO_REMIND, warning)
+  bot.sendMessage(PERSON_TO_WARN, warning)
+}, null, true, 'Europe/Amsterdam')
+
+job4.start()
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id
-
-  if (!userWhitelist.includes(msg.chat.id)) {
-    try {
-      // bot.sendMessage(chatId, 'You cannot use me. It\'s a private bot owned by @useless')
-    } catch (_) { }
-    return
-  }
+  if (!userWhitelist.includes(chatId)) { return }
 
   console.log(msg.chat.id)
 })
